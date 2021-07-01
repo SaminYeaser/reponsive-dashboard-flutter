6d3d40229f407b295c7bf99c02e42d401bc6fd44
SaminYeaser <saminyeaser1@gmail.com>
2021-07-02 01:24:35 +0600

    done

--- lib/screens/components/middleSection/eachContainerSection.dart
+++ lib/screens/components/middleSection/eachContainerSection.dart
@@ -21,63 +21,65 @@ class EachContainerSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 150,
-      width: 200,
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(
-          Radius.circular(10),
+    return Expanded(
+      flex: 1,
+      child: Container(
+       height: 170,
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          ),
+          color: secondaryColor,
         ),
-        color: secondaryColor,
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                child: Padding(
-                  padding: EdgeInsets.all(defaultPadding),
-                  child: SvgPicture.asset(svgPic, height: 20,),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  child: Padding(
+                    padding: EdgeInsets.all(defaultPadding),
+                    child: SvgPicture.asset(svgPic, height: 20,),
+                  ),
+                  decoration: BoxDecoration(
+                    color: color.withOpacity(0.1),
+                    borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
                 ),
-                decoration: BoxDecoration(
-                  color: color.withOpacity(0.1),
-                  borderRadius: BorderRadius.all(Radius.circular(10))
-                ),
-              ),
-              Icon(Icons.more_vert)
-            ],
-          ),
-          Text(
-            titleName,
-          ),
-          Stack(
-            children: [
-              Container(
-                width: double.infinity,
-                height: 5,
-                decoration: BoxDecoration(
-                    color: color.withOpacity(.1),
-                    borderRadius: BorderRadius.circular(10)),
-              ),
-              LayoutBuilder(builder: (context, constraints) {
-                return Container(
-                  width: width,
+                Icon(Icons.more_vert)
+              ],
+            ),
+            Text(
+              titleName,
+            ),
+            Stack(
+              children: [
+                Container(
+                  width: double.infinity,
                   height: 5,
                   decoration: BoxDecoration(
-                      color: color, borderRadius: BorderRadius.circular(10)),
-                );
-              })
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [Text(filesNumber), Text(fileSize)],
-          )
-        ],
+                      color: color.withOpacity(.1),
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+                LayoutBuilder(builder: (context, constraints) {
+                  return Container(
+                    width: width,
+                    height: 5,
+                    decoration: BoxDecoration(
+                        color: color, borderRadius: BorderRadius.circular(10)),
+                  );
+                })
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [Text(filesNumber), Text(fileSize)],
+            )
+          ],
+        ),
       ),
     );
   }
